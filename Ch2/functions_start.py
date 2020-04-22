232e7a9de7f4a2dd1fe2e2446b8986ed8f8cca8d
Trevor Burns <trevor.burns@gmail.com>
2020-04-21 22:27:31 -0400

    LinkedIn functions tutorial

--- Ch2/functions_start.py
+++ Ch2/functions_start.py
@@ -3,39 +3,45 @@
 #
 
 # define a basic function
-def func1():
-    print("I am a function")
+# def func1():
+#     print("I am a function")
+#     f = "w"
+# #     return f
 
-# function that takes arguments
-def func2(arg1,arg2):
-    print(arg1, " ",arg2)
 
-# function that returns a value
-def cube(x):
-    return x*x*x
+# # function that takes arguments
+# def func2(arg1, arg2):
+#     print(arg1, " ", arg2)
 
-# function with default value for an argument
-def power(num, x=1):
-    result = 1
-    for i in range(x):
-        result = result * num
-    return result
 
-#function with variable number of arguments
-def multi_add(*args):
+# # # function that returns a value
+# def cube(x):
+#     return x * x * x
+
+
+# # function with default value for an argument
+# def power(num, x=5):
+#     result = 1
+#     for i in range(x):
+#         result = result * num
+#     return result
+
+
+# function with variable number of arguments
+def multi_add(*fargs):
     result = 0
-    for x in args:
+    for x in fargs:
         result = result + x
     return result
 
 
-#func1()
-#print (func1())
-#print (func1)
-# func2(10,20)
-# print(func2(10,20))
+# func1()
+# print(func1())
+# # print(func1)
+# func2(10, 20)
+# print(func2(10, 20))
 # print(cube(3))
-# print (power(2))
-# print (power(2,3))
-# print (power(x=3, num=2))
-print (multi_add(10, 4, 5, 10, 2))
+# print(power(2))
+# print(power(2, 3))
+# print(power(x=3, num=2))
+print(multi_add(10, 4, 5, 10, 2))
