94cd16949441e90dc3d86a9d33d7ef6b6599f521
Trevor Burns <trevor.burns@gmail.com>
2020-04-21 21:59:20 -0400

    LinkedIn tutorial on variables

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -1,23 +1,27 @@
-# 
+#
 # Example file for variables
 #
-
-# Declare a variable and initialize it
-f=0
-#print(f)
-
-# # re-declaring the variable works
-#f="abc"
-#print(f)
-
-# # ERROR: variables of different types cannot be combined
-
+# import requests
 
 # Global vs. local variables in functions
 def someFunction():
     global f
-    f="def"
+    f = "def"
     print(f)
+
+
+# Declare a variable and initialize it
+f = 0
+print(f)
+
+# # re-declaring the variable works
+f = "abc"
+print(f)
+
+# # ERROR: variables of different types cannot be combined
+f = "this is a string" + str(123)
+print(f)
+
 
 someFunction()
 print(f)
